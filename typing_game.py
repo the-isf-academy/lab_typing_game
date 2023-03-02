01fd3b68a86cea05f2d1f47f2493c94f40b95cf9
emma <eqbrown@isf.edu.hk>
2023-03-02 09:44:20 +0800

    update pseudocode

--- typing_game.py
+++ typing_game.py
@@ -3,23 +3,18 @@ from random import choice
 from helpers import print_slow, calculate_wpm, calculate_accuracy, generate_prompt
 from time import time
 
-####  Welcome Screen
-
 print("-"*50)
-print("Welcome to the ISF Typing Game")
+print("Welcome to the ISF Typing Game")                         # print welcome to the game
 print("-"*50)
 
-#### Game Rules
+input("> Input any key to view the rules.")                     # user input any key to view rules                          
 
-input("> Input any key to view the rules.")
-
-print("     1. You will see the prompt")
+print("     1. You will see the prompt")                        # print rules
 print("     2. The timer will start")
 print("     3. Type the prompt as fast as you can")
 print("     4. Click 'enter/return' when you are done.\n")
 
-#### Game Start Countdown
-input("> Input any key to start the game! ")
+input("> Input any key to start the game! ")                    # user input any key to start game
 
 print("-"*30)
 print("The game will start in...")
@@ -28,11 +23,12 @@ print("-"*30,"\n")
 
 
 #### 💻 YOUR CODE GOES HERE 💻 ####
-# translate this pseudocode into Python code:
-        # store one prompt from prompt_list in chosen_prompt
-        # print chosen_prompt
-        # store current time in start_time
-        # store user typing input attempt in user_typed_prompt 
-        # store current time in end_time 
-        # store the return value of calculated_wpm() in user_wpm
-        # print user_wpm
+#### translate this pseudocode into Python code, feel to change it to make the most sense to you:
+
+# initialize the variable chosen_prompt store a random prompt from  prompt_list 
+# print chosen_prompt
+# initialize the variable start_time store the current time 
+# initialize the variable user_input_prompt store user input prompt typing attempt 
+# initialize the variable end_time store the current time 
+# initialize the variable user_wpm store the return value of calculated_wpm() 
+# print user_wpm
